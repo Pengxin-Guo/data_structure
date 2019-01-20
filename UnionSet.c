7e433607ee7c7bee521d3a95262f4865d7e13c9b
gpx <1457495424@qq.com>
2019-01-20 19:56:37 +0800

    add some files

---
+++ UnionSet.c
@@ -0,0 +1,78 @@
+/*************************************************************************
+	> File Name: UnionSet.c
+	> Author: gpx
+	> Mail: 1457495424@qq.com
+	> Created Time: 2019年01月20日 星期日 16时48分16秒
+ ************************************************************************/
+// quick_find
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct UnionSet {
+    int *color;
+    int size;
+} UnionSet;
+
+UnionSet *init(int n) {
+    UnionSet *u = (UnionSet *)calloc(sizeof(UnionSet), 1);
+    u->color = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++) {
+        u->color[i] = i;
+    }
+    u->size = n;
+    return u;
+}
+
+int find(UnionSet *u, int x) {
+    return u->color[x];
+}
+
+int merge(UnionSet *u, int a, int b) {
+    int color_a = find(u, a), color_b = find(u, b);
+    if (color_a == color_b) return 0;
+    for (int i = 0; i < u->size; i++) {
+        if (u->color[i] == color_a) u->color[i] = color_b;
+    }
+    return 1;
+}
+
+void clear(UnionSet *u) {
+    if (u == NULL) return ;
+    free(u->color);
+    free(u);
+    return ;
+}
+
+void output(UnionSet *u) {
+    for (int i = 0; i < u->size; i++) {
+        printf("(%d, %d)\t", i, u->color[i]);
+        if (i + 1 < u->size && i + 1 % 5 == 0) printf("\n");
+    }
+    printf("\n\n");
+    return ;
+}
+
+int main() {
+    srand(time(0));
+    int op, a, b;
+    #define MAX_OP 20
+    #define MAX_N 10
+    UnionSet *u = init(MAX_N);
+    for(int i = 0; i < MAX_OP; i++) {
+        op = rand() % 4;
+        a = rand() % MAX_N;
+        b = rand() % MAX_N;
+        switch (op) {
+            case 0: {
+                printf("find %d <-> %d = %d\n", a, b, find(u, a) == find(u, b));
+            } break;
+            default: {
+                printf("union %d <-> %d = %d\n", a, b, merge(u, a, b));
+            } break;
+        }
+        output(u);
+    }
+    return 0;
+}
---
+++ UnionSet_2.c
@@ -0,0 +1,78 @@
+/*************************************************************************
+	> File Name: UnionSet_2.c
+	> Author: gpx
+	> Mail: 1457495424@qq.com
+	> Created Time: 2019年01月20日 星期日 17时01分48秒
+ ************************************************************************/
+// quick_union
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct UnionSet {
+    int *father;
+    int size;
+} UnionSet;
+
+UnionSet *init(int n) {
+    UnionSet *u = (UnionSet *)calloc(sizeof(UnionSet), 1);
+    u->father = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++) {
+        u->father[i] = i;
+    }
+    u->size = n;
+    return u;
+}
+
+int find(UnionSet *u, int x) {
+    if (u->father[x] != x) return find(u, u->father[x]);
+    return x;
+}
+
+int merge(UnionSet *u, int a, int b) {
+    int fa = find(u, a), fb = find(u, b);
+    if (fa == fb) return 0;
+    u->father[fa] = fb;
+    return 1;
+}
+
+void clear(UnionSet *u) {
+    if (u == NULL) return ;
+    free(u->father);
+    free(u);
+    return ;
+}
+
+void output(UnionSet *u) {
+    for (int i = 0; i < u->size; i++) {
+        printf("(%d, %d)\t", i, u->father[i]);
+        if (i + 1 < u->size && i + 1 % 5 == 0) printf("\n");
+    }
+    printf("\n\n");
+    return ;
+}
+
+int main() {
+    srand(time(0));
+    int op, a, b;
+    #define MAX_OP 20
+    #define MAX_N 10
+    UnionSet *u = init(MAX_N);
+    for(int i = 0; i < MAX_OP; i++) {
+        op = rand() % 4;
+        a = rand() % MAX_N;
+        b = rand() % MAX_N;
+        switch (op) {
+            case 0: {
+                printf("find %d <-> %d = %d\n", a, b, find(u, a) == find(u, b));
+            } break;
+            default: {
+                printf("union %d <-> %d = %d\n", a, b, merge(u, a, b));
+            } break;
+        }
+        output(u);
+    }
+    return 0;
+}
+
