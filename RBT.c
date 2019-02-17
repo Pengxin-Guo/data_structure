517c8c62d64377668b07c18e6490426f4c593883
gpx <1457495424@qq.com>
2019-02-17 20:55:31 +0800

    add RBT.c

---
+++ RBT.c
@@ -0,0 +1,216 @@
+/*************************************************************************
+	> File Name: RBT.c
+	> Author: gpx
+	> Mail: 1457495424@qq.com
+	> Created Time: 2019年02月17日 星期日 18时07分28秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define RED 0
+#define BLACK 1
+#define DOUBLE_BLACK 2
+
+typedef int color_t;
+
+typedef struct RBTNode {
+    int key;
+    color_t color; // 0 red, 1 black, 2 double black
+    struct RBTNode *lchild, *rchild;
+} RBTNode;
+
+RBTNode *NIL;
+
+__attribute__((constructor))
+void init_NIL() {
+    NIL = (RBTNode *)malloc(sizeof(RBTNode));
+    NIL->key = -1;
+    NIL->color = BLACK;
+    NIL->rchild = NIL->lchild = NIL;
+    return ;
+}
+
+RBTNode *init(int key) {
+    RBTNode *p = (RBTNode *)malloc(sizeof(RBTNode));
+    p->key = key;
+    p->lchild = p->rchild = NIL;
+    p->color = RED;
+    return p;
+}
+
+int has_red_child(RBTNode *root) {
+    return root->lchild->color == RED || root->rchild->color == RED;
+}
+
+RBTNode *left_rotate(RBTNode *node) {
+    RBTNode *temp = node->rchild;
+    node->rchild = temp->lchild;
+    temp->lchild = node;
+    return temp;
+}
+
+RBTNode *right_rotate(RBTNode *node) {
+    RBTNode *temp = node->lchild;
+    node->lchild = temp->rchild;
+    temp->rchild = node;
+    return temp;
+}
+
+RBTNode *insert_maintain(RBTNode *root) {
+    if (!has_red_child(root)) return root;
+    if (root->lchild->color == RED && root->rchild->color == RED) {
+    } else if (root->lchild->color == RED && has_red_child(root->lchild)) {
+        if (root->lchild->rchild->color == RED) {
+            root->lchild = left_rotate(root->lchild);
+        }
+        root = right_rotate(root);
+    }  else if (root->rchild->color == RED && has_red_child(root->rchild)) {
+        if (root->rchild->lchild->color == RED) {
+            root->rchild = right_rotate(root->rchild);
+        }
+        root = left_rotate(root);
+    } else {
+        return root;
+    }
+    root->color = RED;
+    root->lchild->color = root->rchild->color = BLACK;
+    return root;
+}
+
+RBTNode *__insert(RBTNode *root, int key) {
+    if (root == NIL) return init(key);
+    if (root->key == key) return root;
+    else if (root->key > key) {
+        root->lchild = __insert(root->lchild, key);
+    } else {
+        root->rchild = __insert(root->rchild, key);
+    }
+    return insert_maintain(root);
+}
+
+RBTNode *insert(RBTNode *root, int key) {
+    root = __insert(root, key);
+    root->color = BLACK;
+    return root;
+}
+
+RBTNode *predecessor(RBTNode *root) {
+    RBTNode *temp = root->lchild;
+    while (temp->rchild != NIL) temp = temp->rchild;
+    return temp;
+}
+
+RBTNode *erase_maintain(RBTNode *root) {
+    if (root->lchild->color != DOUBLE_BLACK && root->rchild->color != DOUBLE_BLACK) return root;
+    #define UNBALANCE(a, b) (root->a->color == DOUBLE_BLACK && !has_red_child(root->b))
+    if (UNBALANCE(lchild, rchild) || UNBALANCE(rchild, lchild)) {
+        root->color += 1;
+        root->lchild->color -= 1;
+        root->rchild->color -= 1;
+        return root;
+    }
+    #undef UNBALANCE
+    if (root->lchild->color == DOUBLE_BLACK) {
+        if (root->rchild->color == RED) {
+            root = left_rotate(root);
+            root->color = BLACK;
+            root->lchild->color = RED;
+            return erase_maintain(root->lchild);
+        }
+        root->lchild->color = BLACK;
+        if (root->rchild->rchild->color != RED) {
+            root->rchild = right_rotate(root->rchild);
+            root->rchild->color = BLACK;
+            root->rchild->rchild->color = RED;
+        }
+        root = left_rotate(root);
+        root->color = root->lchild->color;
+    } else {
+        if (root->lchild->color == RED) {
+            root = right_rotate(root);
+            root->color = BLACK;
+            root->rchild->color = RED;
+            return erase_maintain(root->rchild);
+        }
+        root->rchild->color = BLACK;
+        if (root->lchild->lchild->color != RED) {
+            root->lchild = left_rotate(root->lchild);
+            root->lchild->color = BLACK;
+            root->lchild->lchild->color = RED;
+        }
+        root = right_rotate(root);
+        root->color = root->rchild->color;
+    }
+    root->lchild->color = root->rchild->color = BLACK;
+    return root;
+}
+
+RBTNode *__erase(RBTNode *root, int key) {
+    if (root == NIL) return NIL;
+    if (root->key > key) {
+        root->lchild = __erase(root->lchild, key);
+    } else if (root->key < key) {
+        root->rchild = __erase(root->rchild, key);
+    } else {
+        if (root->lchild == NIL || root->rchild == NIL) {
+            RBTNode *temp = (root->lchild == NIL ? root->rchild : root->lchild);
+            temp->color += root->color;
+            free(root);
+            return temp;
+        } else {
+            RBTNode *temp = predecessor(root);
+            root->key = temp->key;
+            root->lchild = __erase(root->lchild, temp->key);
+        }
+    }
+    return erase_maintain(root);
+}
+
+RBTNode *erase(RBTNode *root, int key) {
+    root = __erase(root, key);
+    root->color = BLACK;
+    return root;
+}
+
+void clear(RBTNode *node) {
+    if (node == NIL) return ;
+    clear(node->lchild);
+    clear(node->rchild);
+    free(node);
+    return ;
+}
+
+void output(RBTNode *root) {
+    if (root == NIL) return ;
+    printf("(%d %d %d) = %d\n", root->key, root->lchild->key, root->rchild->key, root->color);
+    output(root->lchild);
+    output(root->rchild);
+    return ;
+}
+
+int main() {
+    srand(time(0));
+    int op, val;
+    RBTNode *root = NIL;
+    #define MAX_OP 20
+    for (int i = 0; i < MAX_OP; i++) {
+        op = rand() % 5, val = rand() % 10;
+        switch (op) {
+            case 0:
+                printf("delete val %d from RBT\n", val);
+                root = erase(root, val);
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                printf("insert val %d to RBT\n", val);
+                root = insert(root, val);
+                break;
+        }
+        output(root);
+    }
+    return 0;
+}
